7d6a05cdaf1ee1f44b6595afd7f28d4eb32867cd
Hilson <82933261+Hilsonxhero@users.noreply.github.com>
2022-06-28 17:05:02 +0430

    update x3

--- resources/views/components/landing/service.blade.php
+++ resources/views/components/landing/service.blade.php
@@ -1,10 +1,11 @@
 @props(['services'])
 
-<section class="container pb-5 mt-5 pt-5 mb-md-2 mb-lg-5">
+@if (count($services) > 0)
+    <section class="container pb-5 mt-5 pt-5 mb-md-2 mb-lg-5">
 
-    <h2 class="h1 text-center pt-1 pb-4 mb-1 mb-lg-3">خدمات ما</h2>
-    <div class="row gy-4">
-        {{-- <div class="col-lg-4 text-center text-lg-end pb-3 pb-lg-0 mb-4 mb-lg-0">
+        <h2 class="h1 text-center pt-1 pb-4 mb-1 mb-lg-3">خدمات ما</h2>
+        <div class="row gy-4">
+            {{-- <div class="col-lg-4 text-center text-lg-end pb-3 pb-lg-0 mb-4 mb-lg-0">
             <h2 class="h1 mb-lg-4">لورم ایپسوم متن ساختگی &amp; خدمات</h2>
             <p class="pb-4 mb-0 mb-lg-3">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است،
@@ -14,30 +15,32 @@
 
             <a href="#" class="btn btn-primary shadow-primary btn-lg">مشاهده کامل</a>
         </div> --}}
-        @foreach ($services as $service)
-            <div class="col-lg-3">
-                <div class="card card-hover bg-secondary border-0 mb-4 d-flex flex-col h-100">
-                    <div class="card-body d-flex flex-grow-1 flex-col ">
-                        <div class="d-flex align-items-start">
-                            <div class="flex-shrink-0 bg-light rounded-3 p-3">
-                                <img src="{{ $service->icon }}" width="28" alt="Icon">
+            @foreach ($services as $service)
+                <div class="col-lg-3">
+                    <div class="card card-hover bg-secondary border-0 mb-4 d-flex flex-col h-100">
+                        <div class="card-body d-flex flex-grow-1 flex-col ">
+                            <div class="d-flex align-items-start">
+                                <div class="flex-shrink-0 bg-light rounded-3 p-3">
+                                    <img src="{{ $service->icon }}" width="28" alt="Icon">
+                                </div>
+                                <div class="pe-4">
+                                    <h5 class="h6 pb-2 mb-1">{{ $service->title }}</h3>
+                                        <p class="pb-2 mb-1 text-justify">
+                                            {!! truncate($service->description, 55) !!}
+                                        </p>
+                                        <a href="" class="btn btn-link stretched-link px-0">
+                                            اطلاعات بیشتر
+                                            <i class="bx bx-left-arrow-alt fs-xl ms-2"></i>
+                                        </a>
+                                </div>
                             </div>
-                            <div class="pe-4">
-                                <h5 class="h6 pb-2 mb-1">{{ $service->title }}</h3>
-                                    <p class="pb-2 mb-1 text-justify">
-                                        {!! truncate($service->description, 55) !!}
-                                    </p>
-                                    <a href="" class="btn btn-link stretched-link px-0">
-                                        اطلاعات بیشتر
-                                        <i class="bx bx-left-arrow-alt fs-xl ms-2"></i>
-                                    </a>
-                            </div>
+
+
                         </div>
-
-
                     </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
-</section>
+            @endforeach
+        </div>
+    </section>
+@else
+@endif
--- resources/views/components/landing/treatments.blade.php
+++ resources/views/components/landing/treatments.blade.php
@@ -1,5 +1,74 @@
 @props(['treatments'])
 
+
+@if (count($treatments) > 0)
+    <section class="pb-5 mb-2 mb-md-4 mb-lg-5" style="padding-top: 80px;">
+        <div class="bg-secondary pb-lg-4 pb-xl-5">
+            <div class="container pb-4 pb-md-5">
+                <div class="row">
+
+                    <div class="col-xl-5 col-lg-6 offset-xl-1 pt-2 pt-md-4 pt-lg-5 mt-4">
+
+                        <!-- Slider prev/next buttons -->
+                        <div class="d-flex justify-content-center justify-content-lg-start pb-3 mb-3">
+
+                            <button type="button" id="prev" class="btn btn-prev btn-icon btn-sm ms-2">
+                                <i class="bx bx-chevron-right"></i>
+                            </button>
+                            <button type="button" id="next" class="btn btn-next btn-icon btn-sm me-2">
+                                <i class="bx bx-chevron-left"></i>
+                            </button>
+
+                        </div>
+
+                        <!-- Swiper slider -->
+                        <div class="swiper mx-0 mb-md-n2 mb-xxl-n3"
+                            data-swiper-options='{
+                                                                                              "spaceBetween": 30,
+                                                                                              "loop": true,
+                                                                                              "tabs": true,
+                                                                                              "navigation": {
+                                                                                                "prevEl": "#prev",
+                                                                                                "nextEl": "#next"
+                                                                                              }
+                                                                                            }'>
+                            <div class="swiper-wrapper text-center text-lg-end">
+
+                                @foreach ($treatments as $key => $treatment)
+                                    <div class="swiper-slide pb-4" data-swiper-tab="#image-{{ $key }}">
+                                        <h2 class="h1 mb-4"> {{ $treatment->title }}</h2>
+                                        <p class="pb-2 pb-xl-0 mb-4 mb-xl-5">
+                                            {{ truncate($treatment->description, '120') }}
+
+                                        </p>
+                                        <a href="{{ route('application.treatments.show', $treatment->slug) }}"
+                                            class="btn btn-success btn-lg shadow-success">مشاهده بیشتر</a>
+                                    </div>
+                                @endforeach
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-5 col-lg-6 treatment-content">
+                        <div class="swiper-tabs mx-auto" style="max-width: 526px;">
+                            @foreach ($treatments as $key => $treatment)
+                                <div id="image-{{ $key }}"
+                                    class="swiper-tab  @if ($loop->first) active @endif ">
+                                    <img src="{{ $treatment->banner }}" class="rounded-3 treatment-cover "
+                                        alt="Image" loading="lazy">
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@else
+@endif
+
 @section('css')
     <style>
         .treatment-cover {
@@ -15,68 +84,3 @@
         }
     </style>
 @endsection
-
-<section class="pb-5 mb-2 mb-md-4 mb-lg-5" style="padding-top: 80px;">
-    <div class="bg-secondary pb-lg-4 pb-xl-5">
-        <div class="container pb-4 pb-md-5">
-            <div class="row">
-
-                <div class="col-xl-5 col-lg-6 offset-xl-1 pt-2 pt-md-4 pt-lg-5 mt-4">
-
-                    <!-- Slider prev/next buttons -->
-                    <div class="d-flex justify-content-center justify-content-lg-start pb-3 mb-3">
-
-                        <button type="button" id="prev" class="btn btn-prev btn-icon btn-sm ms-2">
-                            <i class="bx bx-chevron-right"></i>
-                        </button>
-                        <button type="button" id="next" class="btn btn-next btn-icon btn-sm me-2">
-                            <i class="bx bx-chevron-left"></i>
-                        </button>
-
-                    </div>
-
-                    <!-- Swiper slider -->
-                    <div class="swiper mx-0 mb-md-n2 mb-xxl-n3"
-                        data-swiper-options='{
-                                                                                              "spaceBetween": 30,
-                                                                                              "loop": true,
-                                                                                              "tabs": true,
-                                                                                              "navigation": {
-                                                                                                "prevEl": "#prev",
-                                                                                                "nextEl": "#next"
-                                                                                              }
-                                                                                            }'>
-                        <div class="swiper-wrapper text-center text-lg-end">
-
-                            @foreach ($treatments as $key => $treatment)
-                                <div class="swiper-slide pb-4" data-swiper-tab="#image-{{ $key }}">
-                                    <h2 class="h1 mb-4"> {{ $treatment->title }}</h2>
-                                    <p class="pb-2 pb-xl-0 mb-4 mb-xl-5">
-                                        {{ truncate($treatment->description, '120') }}
-
-                                    </p>
-                                    <a href="{{ route('application.treatments.show', $treatment->slug) }}"
-                                        class="btn btn-success btn-lg shadow-success">مشاهده بیشتر</a>
-                                </div>
-                            @endforeach
-
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-5 col-lg-6 treatment-content">
-                    <div class="swiper-tabs mx-auto" style="max-width: 526px;">
-                        @foreach ($treatments as $key => $treatment)
-                            <div id="image-{{ $key }}"
-                                class="swiper-tab  @if ($loop->first) active @endif ">
-                                <img src="{{ $treatment->banner }}" class="rounded-3 treatment-cover " alt="Image"
-                                    loading="lazy">
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
